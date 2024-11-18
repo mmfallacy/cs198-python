2a317c7bafe6be096a34abbdbdda5e1a0c43100b
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2024-11-18 14:10:39 +0800

    feat: include calc function for calculating MTTC graphs

--- hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from cs198-python!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ main.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import colors as pltcolors
+from matplotlib.cm import ScalarMappable
+import math
+X_LIM, Y_LIM, GRAN = 10, 30, 300
+
+# Calculate function
+def calc(fn, vf, max_Z=float('+inf')):
+  # Relative Acceleration
+  X = np.linspace(-X_LIM, X_LIM, GRAN)
+  # Relative Velocity
+  Y = np.linspace(-Y_LIM, Y_LIM, GRAN)
+  Z = np.zeros((GRAN,GRAN))
+  # Create grid of Z values.
+  
+  # Keep track of maximum Z value
+  Z_max = float('-inf')
+
+  # Observe that Z is a 2d-array. Despite being a 2D array, its columns and rows directly translate to the X and Y axes shown in the plot.
+  # Refer to https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.contourf.html.
+  # That is, X pertains to the contourf's columns (hence why it is retrieved using j).
+  # and consequently, Y pertains to the contourf's rows (hence why it is retrieved using i).
+  for i in range(Z.shape[0]):
+    for j in range(Z.shape[1]):
+      # For row i and col j, get corresponding X and Y value
+
+      x,y = X[j], Y[i]
+      # Unit conversion
+      xp, yp = kpsqh2mpsqs(x), kph2mps(y)
+      # Calculate warning distance given y (vrel) and vf
+      wd = fn(y, vf)
+      # If warning distance is impossible, set z to np.nan. Otherwise get MTTC
+      if (wd<0): z = np.nan
+      else: z = MTTC(x,y, wd)
+
+      # Discard nan or negative vl case.
+      # MTTC can output np.nan.
+      # y (vrel) > vf => vf-vl > vf => -vl > 0 => vl < 0.
+      # negative vl is out of our scope.
+      if(y > vf or np.isnan(z)): Z[i,j] = np.nan
+
+      else:
+        # Update maximum Z value
+        if(Z_max < z): Z_max = z
+        # Place computed Z in the grid.
+        Z[i,j] = z
+  
+  # Return maximum Z, linspace X and Y, grid Z
+  return Z_max, X, Y, Z
+
+def main():
+    print("Hello from cs198-python!")
+
+
+if __name__ == "__main__":
+    main()
