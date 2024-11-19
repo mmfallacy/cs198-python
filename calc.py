a9a10f87b22bf52bb7c84f98ba355f2c31d211ea
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2024-11-19 23:24:04 +0800

    chore: remove dead code

--- calc.py
+++ calc.py
@@ -3,53 +3,11 @@ from lib import MTTC, bellarusso, hirstgraham, honda
 
 from os import path, makedirs
 
-from persist import save_calc_csv, save_points_csv
+from persist import save_points_csv
 
 X_LIM, Y_LIM, GRAN = 10, 30, 300
 
 VFS = [3, 6, 12, 18, 36]
-
-# Calculate function
-def old_calc(fn, vf):
-  # Relative Acceleration
-  X = np.linspace(-X_LIM, X_LIM, GRAN)
-  # Relative Velocity
-  Y = np.linspace(-Y_LIM, Y_LIM, GRAN)
-  Z = np.zeros((GRAN,GRAN))
-  # Create grid of Z values.
-  
-  # Keep track of maximum Z value
-  Z_max = float('-inf')
-
-  # Observe that Z is a 2d-array. Despite being a 2D array, its columns and rows directly translate to the X and Y axes shown in the plot.
-  # refer to https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.contourf.html.
-  # That is, X pertains to the contourf's columns (hence why it is retrieved using j).
-  # and consequently, Y pertains to the contourf's rows (hence why it is retrieved using i).
-  for i in range(Z.shape[0]):
-    for j in range(Z.shape[1]):
-      # For row i and col j, get corresponding X and Y value
-
-      x,y = X[j], Y[i]
-      # Calculate warning distance given y (vrel) and vf
-      wd = fn(y, vf)
-      # If warning distance is impossible, set z to np.nan. Otherwise get MTTC
-      if (wd<0): z = np.nan
-      else: z = MTTC(x,y, wd)
-
-      # Discard nan or negative vl case.
-      # MTTC can output np.nan.
-      # y (vrel) > vf => vf-vl > vf => -vl > 0 => vl < 0.
-      # negative vl is out of our scope.
-      if(y > vf or np.isnan(z)): Z[i,j] = np.nan
-
-      else:
-        # Update maximum Z value
-        if(Z_max < z): Z_max = z
-        # Place computed Z in the grid.
-        Z[i,j] = z
-  
-  # Return maximum Z, linspace X and Y, grid Z
-  return Z_max, X, Y, Z
 
 def calc(fn, vf):
   # Relative Acceleration
--- persist.py
+++ persist.py
@@ -1,38 +1,6 @@
 from numpy import asarray
 import json
 
-def save_calc_csv(target, X,Y,Z):
-    
-    # Assertions:
-    # Note on matplotlib's contourf convention of X and Y axes.
-    assert len(Y) == len(Z)
-    assert len(X) == len(Z[0])
-    
-    # Fail if target exists.
-    with open(target, 'x') as out:
-        # Write X as comma separated
-        out.write(",".join(str(i) for i in X))
-        out.write("\n")
-        # Write Y as comma separated
-        out.write(",".join(str(i) for i in Y))
-        out.write("\n")
-        
-        # Write Z for the remainder
-        out.writelines(map(lambda el: ",".join(str(i) for i in el) + "\n" ,Z))
-
-def load_calc_csv(target):
-
-    with open(target) as inp:
-        X = asarray(inp.readline().split(","), dtype=float)
-        Y = asarray(inp.readline().split(","), dtype=float)
-        
-        Z = []
-
-        while line := inp.readline():
-            Z.append(asarray(line.split(","), dtype=float))
-        
-        return X, Y, Z
-    
 def load_simulated(target, SIMULATION_TPS_RUN=120):
     # Note: SIMULATION_TPS_RUN should be set equal to the simulator's TPS parameter.
     # By default the simulator runs the simulations in 120 ticks per second.
