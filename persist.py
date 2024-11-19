e0e6fe10d6770da68405707169e10ac0b550bc89
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2024-11-19 22:07:59 +0800

    feat: loading simulation data, saving simpoints as csv

--- persist.py
+++ persist.py
@@ -1,4 +1,5 @@
 from numpy import asarray
+import json
 
 def save_calc_csv(target, X,Y,Z):
     
@@ -31,3 +32,46 @@ def load_calc_csv(target):
             Z.append(asarray(line.split(","), dtype=float))
         
         return X, Y, Z
+    
+def load_simulated(target, SIMULATION_TPS_RUN=120):
+    # Note: SIMULATION_TPS_RUN should be set equal to the simulator's TPS parameter.
+    # By default the simulator runs the simulations in 120 ticks per second.
+
+    with open(target) as inp:
+        data = json.load(inp)
+
+    # Dexie.js format for exporting IndexedDB
+    rows = data['data']['data'][0]['rows']
+
+    X, Y = [], []
+    Z = {
+        "first_mttc": [],
+        "ave_headway": [],
+        "tick": [],
+        "ave_vx": [],
+    }
+
+    for row in rows:
+        # Skip colliding cases
+        if (row["state_collision"] == "true"): continue
+        # Skip impossible cases
+        if (row["state_first_mttc"] <= 0): continue
+        
+        Y.append(row["params_FV_vx"] - row["params_LV_vx"])
+        X.append(row["params_FV_ax"] - row["params_LV_ax"])
+        
+        Z["first_mttc"].append(row["state_first_mttc"])
+        Z["ave_headway"].append(row["state_ave_headway"])
+        Z["tick"].append(row["state_tick"] / SIMULATION_TPS_RUN)
+        Z["ave_vx"].append(row["state_FV_ave_vx"])
+        
+    return X, Y, Z
+    
+def save_points_csv(target, X, Y, Z):
+    # Assertions:
+    assert len(X) == len(Y) == len(Z)
+
+    with open(target, "x") as out:
+        for i in range(0, len(X)):
+            out.write(f"{X[i]},{Y[i]},{Z[i]}\n")
+        
