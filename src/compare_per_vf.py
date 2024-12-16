2c90874dafbf6c4df0f213a6eb6a281a747d3e47
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2024-12-16 17:17:30 +0800

    feat: catch min and max values of all algorithms and use as norm, show gridlines

--- src/compare_per_vf.py
+++ src/compare_per_vf.py
@@ -1,4 +1,5 @@
 from os import makedirs, path
+import numpy as np
 
 from matplotlib import pyplot as plt
 from matplotlib.cm import ScalarMappable
@@ -6,7 +7,7 @@ from matplotlib.colors import Normalize
 
 from src.const import ALGORITHMS, VFS
 from src.persist import load_points_csv
-from src.plot import add_plot, clip
+from src.plot import add_plot, add_plot_norm, clip
 
 def compare_via_vf():
     # Compare algorithms per vf according to calculated analyses
@@ -14,12 +15,29 @@ def compare_via_vf():
         # Create 1 row 4 col (honda, hirstgraham, bellarusso, color bar)
         fig, axs = plt.subplots(1, 4, figsize=(15,5), width_ratios=[1,1,1,0.1])
 
+        XYZs = {}
+        lowest = float("+inf")
+        highest = float("-inf")
+
+        for algo in ALGORITHMS:
+            XYZ = clip(load_points_csv(f"calculated/{algo.__name__}/{vf}.csv"))
+            lowest = min(lowest, np.nanmin(XYZ[2]))
+            highest = max(highest, np.nanmax(XYZ[2]))
+            XYZs[algo.__name__] = XYZ
+        
+        # Set lowest to 0
+        lowest = 0
+        # Set highest to 7
+        highest = 4
+
+        norm = Normalize(vmin=lowest, vmax=highest)
+
         # For each algorithm
         for i, algo in enumerate(ALGORITHMS):
             # Load points
-            XYZ = load_points_csv(f"calculated/{algo.__name__}/{vf}.csv")
+            XYZ = XYZs[algo.__name__]
             # Create plot (scatter plot, since points are loaded)
-            add_plot(fig, axs[i], *clip(XYZ), 7)
+            add_plot_norm(fig, axs[i], *XYZ, norm, None)
 
             # Differentiate subplots based on algo name
             axs[i].set_title(algo.__name__)
@@ -35,7 +53,7 @@ def compare_via_vf():
         fig.suptitle(f"MTTC vs dA and dV (vf={vf})")
 
         # Create and add color bar
-        sm = ScalarMappable(cmap='RdYlGn', norm=Normalize(vmin=0, vmax=7))
+        sm = ScalarMappable(cmap='RdYlGn', norm=norm)
         fig.colorbar(sm, cax=axs[3])
 
         fig.tight_layout()
--- src/plot.py
+++ src/plot.py
@@ -14,7 +14,7 @@ def clip(XYZ, filter=iqrfilter):
 
 def add_plot(fig, ax, X,Y,_Z, max, cbar=False):
     Z = np.clip(_Z, None, max)
-    ax.scatter(X,Y, c=Z, cmap="RdYlGn")
+    ax.scatter(X,Y, s=1, c=Z, cmap="RdYlGn")
     sm = ScalarMappable(cmap='RdYlGn', norm=Normalize(vmin=np.nanmin(Z), vmax=np.nanmax(Z)))
 
     if(cbar): fig.colorbar(sm, ax=ax)
@@ -31,4 +31,23 @@ def add_plot(fig, ax, X,Y,_Z, max, cbar=False):
         yp = Y[idx]
         return 'x={x:.5f}  y={y:.5f}  z={z:.5f}'.format(x=xp, y=yp, z=z)
     ax.format_coord = fmt
-      
+
+def add_plot_norm(fig,ax, X,Y,Z, norm, cbar=False):
+    ax.scatter(X,Y, s=1, c=Z, norm=norm, cmap="RdYlGn")
+    ax.grid(True)
+
+    sm = ScalarMappable(cmap='RdYlGn', norm=norm)
+    if(cbar): fig.colorbar(sm, ax=ax)
+    ax.set_facecolor("black")
+    ax.set_xlim(-X_LIM, X_LIM)
+    ax.set_ylim(-Y_LIM,Y_LIM)
+
+    def fmt(x, y):
+        # get closest point with known data
+        dist = np.linalg.norm(np.vstack([X - x, Y - y]), axis=0)
+        idx = np.argmin(dist)
+        z = Z[idx]
+        xp = X[idx]
+        yp = Y[idx]
+        return 'x={x:.5f}  y={y:.5f}  z={z:.5f}'.format(x=xp, y=yp, z=z)
+    ax.format_coord = fmt
