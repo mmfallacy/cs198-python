4ecacacd19099fca60d5e303348e249206de4f27
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-02-24 22:29:32 +0800

    feat: show=zdist function

---
+++ src/v2/show_zdist.py
@@ -0,0 +1,44 @@
+
+from os import makedirs, path
+from matplotlib import pyplot as plt
+from src.const import ALGORITHMS
+from src.persist import load_points_csv
+
+
+def show_zdist(vf, metrics):
+  '''
+    This function displays a specific metric as follows:
+                |  metric[0]  |  ...  |  metric[-1]  |
+    honda
+    hirstgraham
+    bellarusso
+  '''
+
+  n = len(metrics)
+  fig, axs = plt.subplots(3, n, figsize=(3*n, 10))
+  
+    
+  for i, algo in enumerate(ALGORITHMS):
+    for j, metric in enumerate(metrics):
+      _,_,Z = load_points_csv(f"plots/{algo.__name__}-vf={vf}/{metric}.csv")
+      
+      ax = axs[i][j]
+      ax.hist(Z, bins=200)
+      ax.set_xlabel(metric)
+
+    axs[i][0].set_ylabel(algo.__name__)
+  
+  metstr = ",".join(metrics)
+  fig.suptitle(f"vf={vf} value distribution for {metstr}")
+  fig.tight_layout()
+  return fig
+      
+def run_show_zdist(vf, metrics, showPlot=False): 
+  fig = show_zdist(vf, metrics)
+  out_dir = path.join("assets")
+  makedirs(out_dir, exist_ok=True)
+  metstr = ",".join(metrics)
+  out = path.join(out_dir, f"show=zdist vf={vf} metrics={metstr}.png")
+
+  fig.savefig(out)
+  if showPlot: plt.show()
