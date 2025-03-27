ef0e7a3c21b2547980ab8fb6dc849578060bc093
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-03-27 22:38:27 +0800

    docs: document why it is necessary to unset PYTHONPATH

--- nix/devShell.nix
+++ nix/devShell.nix
@@ -20,6 +20,9 @@ pkgs.mkShell {
     };
 
   shellHook = ''
+    # undo dependency propagation from nixpkgs 
+    # [1](https://nixos.org/manual/nixpkgs/stable/#setup-hook-python) 
+    # [2](https://github.com/NixOS/nixpkgs/blob/b841c624fda46a8e28a007684eb56d407fb246b8/pkgs/development/interpreters/python/setup-hook.sh)
     unset PYTHONPATH
   '';
 }
